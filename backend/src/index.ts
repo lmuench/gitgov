f309516250265fee42410ca16a75d1593b9b89af
Ludwig Muench <muench.ludwig@gmail.com>
2019-01-04 14:41:27 +0100

    move everything into backend folder, so frontend can be in same repo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -40,6 +40,6 @@ createConnection().then(async connection => {
   ]
   await poll.save();
 
-  console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+  console.log("Express server has started on port 3000.");
 
 }).catch(error => console.log(error));
